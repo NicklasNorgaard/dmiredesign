b44f574215fc7191d3db811b775fa1f1895e57d4
NicklasNorgaard <kontakt@nicklasnorgaard.org>
2021-03-03 17:53:41 +0100

    Varselsprognose færdiggjort

--- varsler.php
+++ varsler.php
@@ -30,42 +30,68 @@
 <!-- i <body> har man alt indhold på siden som brugeren kan se -->
 <body>
 <?php include "includes/menu.php"; ?>
+<div class="container-fluid">
 <h1>Varsler</h1>
 <p>Her på siden kan du følge med i, hvilke varsler der er i dit område. Varslerne dækker over dagen i dag, og de næste 5 dage. Vær opmærksom på at vores varsler kun er vejledende. Hold dig opdateret gennem radio/tv,
     og kontakt Vejdirektoratet hvis du er i tvivl om hvor vidt der er forsvarligt at bevæge dig ud.</p>
 <br>
-<div class="container list-group shadow">
-    <div class="row">
-        <div class="list-group-item col-md-2" style="background: linear-gradient(#A0FFA1, #FFB899);">
-        <span><i class="fas fa-map-marker-alt"></i><h3>Slagelse</h3></span>
-        <span><h3>Mandag</h3><img src="images/kategori0.png"><h4>Kategori 0</h4><p>Ingen varsler</p></span>
-        </div>
-
-        <div class="list-group-item col-md" style="background: linear-gradient(#A0FFA1, #FFB899);">
-        <span><h3>Tirsdag</h3><img src="images/kategori0.png" style="height: 60px; width: 60px"><h4>Kategori 0</h4><p>Ingen varsler</p></span>
-        </div>
-
-        <div class="list-group-item col-md" style="background: linear-gradient(#A0FFA1, #FFB899);">
-        <span><h3>Onsdag</h3><img src="images/kategori0.png" style="height: 60px; width: 60px"><h4>Kategori 0</h4><p>Ingen varsler</p></span>
-        </div>
-
-        <div class="list-group-item col-md" style="background: linear-gradient(#A0FFA1, #FFB899);">
-        <span><h3>Torsdag</h3><img src="images/kategori0.png" style="height: 60px; width: 60px"><h4>Kategori 0</h4><p>Ingen varsler</p></span>
-        </div>
-
-        <div class="list-group-item col-md" style="background: linear-gradient(#F1FF93, #FFB899);">
-            <span><h3>Fredag</h3><img src="images/kategori2.png" style="height: 60px; width: 60px"><img src="images/varsel_vind.png" style="height: 60px; width: 60px"><h4>Kategori 2</h4><p>Stærk storm</p><p>12:45 - </p></span>
-        </div>
-
-        <div class="list-group-item col-md" style="background: linear-gradient(#F1FF93, #FFB899);">
-        <span><h3>Lørdag</h3><img src="images/kategori2.png" style="height: 60px; width: 60px"><img src="images/varsel_vind.png" style="height: 60px; width: 60px"><h4>Kategori 2</h4><p>Stærk storm </p><p>Hele dagen</p></span>
-        </div>
-
-        <div class="list-group-item col-md" style="background: linear-gradient(#B4D2FF, #FFB899);">
-            <h3>Søndag</h3><img src="images/kategori1.png" style="height: 60px; width: 60px"><img src="images/varsel_vind.png" style="height: 60px; width: 60px"><h4>Kategori 1</h4><p>Storm</p><p> - 20:00</p>
-        </div>
+<div class="vejrboks shadow">
+    <div class="varsler-idag">
+        <div id="v-by"><p><i class="fas fa-map-marker-alt"></i> Slagelse</p></div>
+        <p id="v-dag-idag">Mandag</p>
+        <div id="symbol-idag"><img src="images/kategori0.png"></div>
+        <p id="kategori-idag">Kategori 0</p>
+        <p id="varsel-idag">Ingen varsler</p>
+        <p id="varighed-idag">Hele dagen</p>
+        <br>
+    </div>
+    <div class="varsler-næste" id="v-tirs">
+        <p class="v-dag">Tirsdag</p>
+        <div class="symbol"><img src="images/kategori0.png"></div>
+        <p class="kategori">Kategori 0</p>
+        <p class="varsel">Ingen varsler</p>
+        <p class="varighed">Hele dagen</p>
+    </div>
+    <div class="varsler-næste" id="v-ons">
+        <p class="v-dag">Onsdag</p>
+        <div class="symbol"><img src="images/kategori0.png"></div>
+        <p class="kategori">Kategori 0</p>
+        <p class="varsel">Ingen varsler</p>
+        <p class="varighed">Hele dagen</p>
+    </div>
+    <div class="varsler-næste" id="v-tors">
+        <p class="v-dag">Torsdag</p>
+        <div class="symbol"><img src="images/kategori0.png"></div>
+        <p class="kategori">Kategori 0</p>
+        <p class="varsel">Ingen varsler</p>
+        <p class="varighed">Hele dagen</p>
+    </div>
+    <div class="varsler-næste" id="v-fre">
+        <p class="v-dag">Fredag</p>
+        <div class="symbol"><img src="images/kategori2.png"></div>
+        <div class="symbol2"><img src="images/varsel_vind.png"></div>
+        <p class="kategori">Kategori 2</p>
+        <p class="varsel">Stærk storm</p>
+        <p class="varighed">12:45 - </p>
+    </div>
+    <div class="varsler-næste" id="v-lør">
+        <p class="v-dag">Lørdag</p>
+        <div class="symbol"><img src="images/kategori2.png"></div>
+        <div class="symbol2"><img src="images/varsel_vind.png"></div>
+        <p class="kategori">Kategori 2</p>
+        <p class="varsel">Stærk storm</p>
+        <p class="varighed">Hele dagen</p>
+    </div>
+    <div class="varsler-næste" id="v-søn">
+        <p class="v-dag">Søndag</p>
+        <div class="symbol"><img src="images/kategori1.png"></div>
+        <div class="symbol2"><img src="images/varsel_vind.png"></div>
+        <p class="kategori">Kategori 2</p>
+        <p class="varsel">Storm</p>
+        <p class="varighed"> - 20:00</p>
     </div>
 </div>
+
 <br>
 <h1>Forstå DMIs varsler</h1>
 <div class="container-md list-group shadow">
@@ -99,6 +125,7 @@
 <h1>Få varsler direkte på mobilen</h1>
 <p>Med DMIs app får du push-notifikationer, hver gang en ny varsel opstår. Samtidig vil du på vores app kunne få alt den information du behøver, for at opholde sig så sikkert som muligt. Appen er gratis og kan hentes i både <a href="https://apps.apple.com/dk/app/dmi-vejr/id533069944">App Store</a> og <a href="https://play.google.com/store/apps/details?id=dk.dmi.byvejret&hl=da&gl=US">Google Play Store</a>. </p>
 <br>
+</div>
 <?php include "includes/footer.php"; ?>
 </body>
 </html>
